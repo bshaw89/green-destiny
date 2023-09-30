238d7217056743fd34386d4848e616d65ba30a91
Ben <shaw.j.benjamin@gmail.com>
2023-09-29 20:30:07 -0400

    Setup background and camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform subject;
+    public float smoothSpeed;
+    public Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 targetPosition = subject.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
     }
+  
 }
